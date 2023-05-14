bb786bf27043c728aa2e9b0e8d0f37e358966670
joayeon <whffu0156@gmail.com>
2023-05-14 23:50:19 +0900

    Refactor: 코드 수정

--- 코딩테스트 입문/5월/9일/문자 반복 출력하기.js
+++ 코딩테스트 입문/5월/9일/문자 반복 출력하기.js
@@ -1,4 +1,3 @@
 function solution(my_string, n) {
-    let answer = [...my_string].map((v) => v.repeat(n)).join('');
-    return answer;
+    return [...my_string].map((v) => v.repeat(n)).join('');
 }
