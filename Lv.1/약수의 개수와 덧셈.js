23164ac68deefddd36bdf6959fc3e44879a191bc
joayeon <codinz.n@gmail.com>
2023-06-17 13:22:04 +0900

    :sparkles:  add 약수의 개수와 덧셈

---
+++ Lv.1/약수의 개수와 덧셈.js
@@ -0,0 +1,16 @@
+function solution(left, right) {
+    let ans = 0;
+
+    for (let i = left; i <= right; i++) {
+        let arr = [];
+
+        for (let j = 1; j <= i; j++) {
+            if (i % j == 0) arr.push(j);
+        }
+
+        if (arr.length % 2 != 0) ans -= i;
+        else ans += i;
+    }
+
+    return ans;
+}
